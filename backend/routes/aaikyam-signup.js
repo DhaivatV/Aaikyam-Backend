1e1dc3498e42ce9b939b292801ecd9c62bfd7ffc
DhaivtV <vipatdhaivat25@gmail.com>
2023-06-29 20:06:04 +0530

    signup functional need to add bycrypt

--- backend/routes/aaikyam-signup.js
+++ backend/routes/aaikyam-signup.js
@@ -5,55 +5,60 @@ const { MongoClient } = require("mongodb");
 const router = express.Router();
 
 
-const uri = "mongodb+srv://dv:dv123@aaikyam.pehbz3m.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri);
-client.connect();
-const database = client.db('aaikyam_signup_users');
-const userCollection = database.collection('user_data');
 
-const users = userCollection.find().toArray();
     
 
 
-router.post('/aaikyam', (req, res) => {
+router.post('/aaikyam', async function (req, res) {
   const { username, email, password } = req.body;
+  const uri = "mongodb+srv://dv:dv123@aaikyam.pehbz3m.mongodb.net/?retryWrites=true&w=majority";
+  const client = new MongoClient(uri);
 
-  if (!username || !email || !password) {
-    return res.status(400).json({ error: 'Missing required fields' });
+  try {
+    await client.connect();
+    const database = client.db('aaikyam_signup_users');
+    const userCollection = database.collection('user_data');
+
+    const users = await userCollection.find().toArray();
+    console.log(users);
+
+    if (!username || !email || !password) {
+      return res.status(400).json({ error: 'Missing required fields' });
+    }
+
+    if (users.length == 0) {
+      const newUser = {
+        username,
+        email,
+        password,
+      };
+
+      await userCollection.insertOne(newUser);
+      return res.status(409).json({ error: 'New User Added' });
+    }
+
+    const existingUser = await userCollection.findOne({
+      $or: [{ username: username }, { email: email }],
+    });
+
+    if (existingUser) {
+      return res.status(409).json({ msg: "Username or email already exists" });
+    } else {
+      const newUser = {
+        username,
+        email,
+        password,
+      };
+
+      await userCollection.insertOne(newUser);
+      return res.status(409).json({ msg: 'New User Added' });
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  } finally {
+    await client.close();
   }
-
-
-  if (users.length ==0)
-  {
-    const newUser = {
-      username,
-      email,
-      password
-    };
-
-    userCollection.insertOne(newUser);
-    return res.status(409).json({ error: 'New User Added' })
-  }
-
-  if (users.some(user => user.username === username || user.email === email)) {
-    return res.status(409).json({ error: 'Username or email already exists' });
-  }
-
-  else{
-    const newUser = {
-      username,
-      email,
-      password
-    };
-
-    userCollection.insertOne(newUser);
-    return res.status(409).json({ error: 'New User Added' })
-  }
-
-  
-
-
 });
-   
 
 module.exports = router;
